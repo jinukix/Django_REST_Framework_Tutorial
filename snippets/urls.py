5592740d0b50727a28c1a0d2e9585b4ff97d6bab
장진욱 <wfo3977@gmail.com>
2021-03-14 17:56:01 +0900

    Requests and Responses

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-from snippets    import views
+from django.conf.urls import url
+from django.urls                import path
+from snippets                   import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
 ]
+
+
+urlpatterns = format_suffix_patterns(urlpatterns)
