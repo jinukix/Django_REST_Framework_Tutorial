aec0a1f010876c0a7543fff432bd8e7201c84d95
장진욱 <wfo3977@gmail.com>
2021-03-15 01:51:03 +0900

    Authentication and permissions

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,7 +1,19 @@
-from rest_framework import serializers
-from .models        import Snippet
+from rest_framework             import serializers
+from rest_framework.utils import model_meta
+from .models                    import Snippet
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many = True, qureyset = Snippet.objects.all())
+
+    class Meta:
+        model  = User
+        fields = ['id', 'username', 'snippets']
+
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source = 'owner.username')
+    
     class Meta:
         model  = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', "owner"]
--- snippets/views.py
+++ snippets/views.py
@@ -1,12 +1,15 @@
-from django.db.models import query
-from snippets.models         import Snippet
-from snippets.serializers    import SnippetSerializer
-from django.http             import Http404
-from rest_framework.views    import APIView
-from rest_framework.response import Response
-from rest_framework          import status
-from rest_framework          import mixins
-from rest_framework          import generics
+from django.db.models           import query
+from snippets.models            import Snippet
+from snippets.serializers       import SnippetSerializer
+from django.http                import Http404
+from rest_framework.views       import APIView
+from rest_framework.response    import Response
+from rest_framework             import status
+from rest_framework             import mixins
+from rest_framework             import generics
+from django.contrib.auth.models import User
+from snippets.serializers       import UserSerializer
+from rest_framework             import permissions
 
 """ Class-based view """
 # class SnippetList(APIView):
@@ -82,9 +85,24 @@ from rest_framework          import generics
 """ Use Generic Class-based View """
 
 class SnippetList(generics.ListCreateAPIView):
-    queryset         = Snippet.objects.all()
-    serializer_class = SnippetSerializer
+    queryset           = Snippet.objects.all()
+    serializer_class   = SnippetSerializer
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner = self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset         = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+""" User View """
+
+class UserList(generics.ListAPIView):
+    queryset         = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset         = User.objects.all()
+    serializer_class = UserSerializer
